185c92bcf6e97e9748f860fd0c29f7e836ad27eb
Abdulaziz <suleymanovabdulaziz@gmail.com>
2024-02-16 16:27:37 +0000

    added models in entity

---
+++ src/main/java/com/products/eCommApplication/demo/entity/Product.java
@@ -0,0 +1,35 @@
+package com.products.eCommApplication.demo.entity;
+
+public class Product {
+    private Long id;
+    private String name;
+    private double price;
+
+    public Product(Long id, String name, double price) {
+        this.name = name;
+        this.price = price;
+        this.id = id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public double getPrice() {
+        return price;
+    }
+}
